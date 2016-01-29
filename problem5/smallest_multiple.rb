5ea0c357ac046cc399428cb0e82ad09765af3951
Yasunari Kondo <kyasunari123v@gmail.com>
2016-01-30 00:45:05 +0900

    add problem sentence

--- problem5/smallest_multiple.rb
+++ problem5/smallest_multiple.rb
@@ -1,3 +1,11 @@
+=begin
+Problem 5 「最小の倍数」
+
+2520 は 1 から 10 の数字の全ての整数で割り切れる数字であり, そのような数字の中では最小の値である.
+
+では, 1 から 20 までの整数全てで割り切れる数字の中で最小の正の数はいくらになるか.
+=end
+
 module SmallestMultiple
   def self.start
     target_number = 20
