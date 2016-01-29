c3b64b8c86a57f7a77d612aa1cb0270c3c161098
Yasunari Kondo <kyasunari123v@gmail.com>
2016-01-30 00:30:48 +0900

    delete sentence when debug

--- problem4/palindromic_number.rb
+++ problem4/palindromic_number.rb
@@ -1,3 +1,8 @@
+=begin
+左右どちらから読んでも同じ値になる数を回文数という. 2桁の数の積で表される回文数のうち, 最大のものは 9009 = 91 × 99 である.
+では, 3桁の数の積で表される回文数の最大値を求めよ.
+=end
+
 module Palindrome
   def self.start
     products = []
@@ -7,7 +12,7 @@ module Palindrome
         products << product if palindrome? product
       end
     end
-    p products.max == 906609
+    puts products.max
   end
 
   def self.palindrome? a
