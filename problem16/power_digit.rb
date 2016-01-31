6b3b4f451cf056e841055a96b4b46d1fa0006671
Yasunari Kondo <kyasunari123v@gmail.com>
2016-01-31 21:46:03 +0900

    refactor problem16

--- problem16/power_digit.rb
+++ problem16/power_digit.rb
@@ -1,12 +1,16 @@
 module PowerDigit
   def self.start
-    num = 2 ** 1000
-    num_arr =  num.to_s.split('').map(&:to_i)
+    num = 2**1000
+    num_arr = num.to_s.split('').map(&:to_i)
+    puts sum_arr(num_arr)
+  end
+
+  def self.sum_arr(arr)
     sum = 0
-    num_arr.each do |n|
+    arr.each do |n|
       sum += n
     end
-    puts sum
+    sum
   end
 end
 
