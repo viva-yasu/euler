6f9d92dc642101b18848d298c471417c5d309449
Yasunari Kondo <kyasunari123v@gmail.com>
2016-01-31 21:46:50 +0900

    add problem sentence to problem16

--- problem16/power_digit.rb
+++ problem16/power_digit.rb
@@ -1,3 +1,10 @@
+=begin
+Problem 16 「べき乗の数字和」
+
+215 = 32768 であり, これの数字和 ( 各桁の和 ) は 3 + 2 + 7 + 6 + 8 = 26 となる.
+同様にして, 21000 の数字和を求めよ.
+=end
+
 module PowerDigit
   def self.start
     num = 2**1000
