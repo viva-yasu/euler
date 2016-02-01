f04db87bc2b7dc4bd73735d1f801d246ae79b106
Yasunari Kondo <kyasunari123v@gmail.com>
2016-02-01 09:40:22 +0900

    refactor problem 19

--- problem19/counting_sundays.rb
+++ problem19/counting_sundays.rb
@@ -3,8 +3,8 @@ require 'date'
 module CountingSundays
   def self.start
     sunday_counter = 0
-    1901.upto 2000 do |year|
-      1.upto 12 do |month|
+    1901.upto(2000) do |year|
+      1.upto(12) do |month|
         target_month = Date.new(year, month, 1)
         if target_month.wday == 0
           sunday_counter += 1
