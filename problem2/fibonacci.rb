f02f356069d1395e790677a99228c6bf419c4ff4
Yasunari Kondo <kyasunari123v@gmail.com>
2016-02-02 06:53:53 +0900

    use `inject` instead of each

--- problem2/fibonacci.rb
+++ problem2/fibonacci.rb
@@ -10,18 +10,17 @@ Problem 2 「偶数のフィボナッチ数」
 module Fibonacci
   def self.start
     nums = [1, 2]
+
     while nums[-2] + nums[-1] < 4000000
       nums.push(nums[-2] + nums[-1])
     end
 
-     puts sum_even(nums)
+    puts sum_even(nums)
   end
 
-  def self.sum_even target_arr
-    sum = 0
+  def self.sum_even(target_arr)
     target_arr.select!(&:even?)
-    target_arr.each { |n| sum += n }
-    sum
+    target_arr.inject(:+)
   end
 end
 
