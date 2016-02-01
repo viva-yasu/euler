00c937f21d170693e73d624026c6309ced6af3cb
Yasunari Kondo <kyasunari123v@gmail.com>
2016-02-01 10:12:11 +0900

    refactor problem 20

--- problem20/factorial_digit_sum.rb
+++ problem20/factorial_digit_sum.rb
@@ -1,26 +1,20 @@
-module FactorialDigitSum
-  def self.start target_number
-    _factorial = factorial(target_number)
-    sum_factorial_digit = sum_digit(_factorial)
-    p sum_factorial_digit
-  end
-
-  def self.factorial n
-    _factorial = 1
-    n.downto(1) do |i|
-      _factorial *= i
-    end
-    _factorial
-  end
-
-  def self.sum_digit n
-    n = n.to_s.split('').map(&:to_i)
-    sum = 0
-    n.each do |i|
-      sum += i
-    end
-    sum
+class Integer
+  def factorial
+    (1..self).inject(1,:*)
   end
 end
 
-FactorialDigitSum.start 100
+module FactorialDigitSum
+  def self.start target_number
+    factorial = target_number.factorial
+    sum_factorial_digit = sum_digit(factorial)
+    puts sum_factorial_digit
+  end
+
+  def self.sum_digit(n)
+    n = n.to_s.split('').map(&:to_i)
+    sum = n.inject(:+)
+  end
+end
+
+FactorialDigitSum.start(100)
