28b97da799e385357d1c93d12368f8c193a4ce7c
Yasunari Kondo <kyasunari123v@gmail.com>
2016-02-02 07:22:04 +0900

    refactor problem6 #2

--- problem6/square.rb
+++ problem6/square.rb
@@ -3,10 +3,10 @@ Problem 6 「二乗和の差」
 
 最初の10個の自然数について, その二乗の和は,
 
-12 + 22 + ... + 102 = 385
+1**2 + 2**2 + ... + 10**2 = 385
 最初の10個の自然数について, その和の二乗は,
 
-(1 + 2 + ... + 10)2 = 3025
+(1 + 2 + ... + 10)**2 = 3025
 これらの数の差は 3025 - 385 = 2640 となる.
 
 同様にして, 最初の100個の自然数について二乗の和と和の二乗の差を求めよ.
@@ -15,22 +15,17 @@ Problem 6 「二乗和の差」
 module Square
 
   def self.start
-    puts square_of_sum_1_to_100 - sum_of_square_1_to_100
+    puts square_of_sum(1, 100) - sum_of_square(1, 100)
   end
 
-  def self.sum_of_square_1_to_100
-    sum = 0
-    1.upto(100) do |n|
+  def self.sum_of_square(a, b)
+    (a..b).inject(0) do |sum, n|
       sum += n ** 2
     end
-    sum
   end
 
-  def self.square_of_sum_1_to_100
-    sum = 0
-    1.upto(100) do |n|
-      sum += n
-    end
+  def self.square_of_sum(a, b)
+    sum = (a + b) * (b - a + 1) / 2
     sum ** 2
   end
 end
