e98f5d8d182751259d3e4796469a46a3d7ed5a70
Yasunari Kondo <kyasunari123v@gmail.com>
2016-02-02 07:05:47 +0900

    refactor problem4

--- problem4/palindromic_number.rb
+++ problem4/palindromic_number.rb
@@ -11,13 +11,13 @@ module Palindrome
     999.downto(100) do |i|
       999.downto(100) do |j|
         product = i * j
-        products << product if palindrome? product
+        products << product if palindrome?(product)
       end
     end
     puts products.max
   end
 
-  def self.palindrome? a
+  def self.palindrome?(a)
     a.to_s == a.to_s.reverse
   end
 end
