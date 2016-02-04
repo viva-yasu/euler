ad908480b4d321450fda47ab7b6e57834be35996
Yasunari Kondo <kyasunari123v@gmail.com>
2016-02-05 02:51:26 +0900

    fix #1

--- problem21/amicable_numbers.rb
+++ problem21/amicable_numbers.rb
@@ -1,42 +1,24 @@
+class Integer
+  def divs
+    rti = (self**0.5).to_i
+    ans = (1..rti).select{|x| self % x == 0}
+    ans.concat(ans.map{|x| self / x}.reverse)
+    ans.uniq
+  end
+
+  def amicable?
+    _self = self
+    was = _self.divs.inject(:+) - _self
+    wat = was.divs.inject(:+) - was
+    return true if wat == _self and _self != was
+    false
+  end
+
+end
+
 module Amicable
   def self.start
-    amicable_numbers = []
-    200.upto(300) do |n|
-      n_divs = {}
-      n_sum_div = sum_divisors(get_divisors(n))
-      n_divs.store(n, n_sum_div)
-      200.upto(300) do |i|
-        next if n == i
-        i_sum_div = sum_divisors(get_divisors(i))
-        if n_divs[i_sum_div]
-
-        end
-      end
-    end
-    p amicable_numbers
-  end
-  def self.get_divisors int
-    divisors = []
-    1.upto(int - 1) do |n|
-      if int.modulo(n).zero?
-        divisors << n
-      end
-    end
-    divisors
-  end
-  def self.sum_divisors target_number
-    sum = 0
-    target_number.each do |n|
-      sum += n
-    end
-    sum
-  end
-  def self.sum_amicable_numbers amicable_numbers
-    sum = 0
-    amicable_numbers.each do |n|
-      sum += n
-    end
-    sum
+    puts (2..10000).select{|x| x.amicable?}.inject(:+)
   end
 end
 
