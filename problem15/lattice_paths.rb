5f285924d844d62cdaa9c8aa0e49d87f0b489618
Yasunari Kondo <kyasunari123v@gmail.com>
2016-02-06 21:58:47 +0900

    add problem15 sentence

--- problem15/lattice_paths.rb
+++ problem15/lattice_paths.rb
@@ -1,3 +1,10 @@
+=begin
+Problem 15 「格子経路」 †
+2×2 のマス目の左上からスタートした場合, 引き返しなしで右下にいくルートは 6 つある.
+
+では, 20×20 のマス目ではいくつのルートがあるか.
+=end
+
 module LatticePaths
   def self.start(n, r)
     puts combination(n, r)
